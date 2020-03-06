cab848a5c98d55e84e82a6656f1b16700f68c353
Jiyoon Bak <wldbs204@gmail.com>
2020-03-06 23:29:39 +0900

    Send Slack Message Success

--- slackbot/main.py
+++ slackbot/main.py
@@ -3,45 +3,49 @@ import json
 import random
 from datetime import date
 
+class UserInfo():
+    def __init__(self):
 
-
-
-def make_query(user_info):
-    headers = {
-    "Authorization": "Bearer ",
-    "Content-Type": "application/json", 
-    "Accept": "application/vnd.github.inertia-preview+json"
-    }
-
-    # Graphql query 엑세스 값
-    query = """
-    query{
-    user(login: "jiyoonbak") {
-        contributionsCollection(from: "2020-03-02T00:00:00.000+00:00", to: "2020-03-02T00:00:00.000+00:00") {
-        totalCommitContributions
+    def make_github_query(self):
+        headers = {
+        "Authorization": "Bearer {}".format(self.git_token),
+        "Content-Type": "application/json", 
+        "Accept": "application/vnd.github.inertia-preview+json"
         }
-    }
-    }
-    """
 
-def run_query(query):
-    # GitHub Request
-    git_response = requests.post("https://api.github.com/graphql", json={'query':query}, headers=headers)
-    if git_response.status_code == 200: # Request code 200 means ok.
-        return git_response.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(git_response.status_code, query))
+        # Graphql query 엑세스 값
+        query = """
+        query{{
+        user(login: "{0}") {{
+            contributionsCollection(from: "{1}", to: "{2}") {{
+            totalCommitContributions
+            }}
+        }}
+        }}
+        """.format(self.git_user_name, date.today().strftime("%Y-%m-%dT%H:%M:%S"), date.today().strftime("%Y-%m-%dT%H:%M:%S"))
 
-def send_slack(msg):
-    # 슬랙 전송
-    slack_url = ""
-    headers = {"Content-Type": "application/json"}
-    data = f"{{'text':'{msg}', 'token':''}}"
-    data = data.encode(encoding='utf-8')
+        return headers, query
 
-    requests.post(slack_url, data=data, headers=headers)
+    def request_github(self):
+        # GitHub Request
+        headers, query = self.make_github_query()
+        git_response = requests.post("https://api.github.com/graphql", json={'query':query}, headers=headers)
+        if git_response.status_code == 200: # Request code 200 means ok.
+            return git_response.json()
+        else:
+            raise Exception("Query failed to run by returning code of {}. {}".format(git_response.status_code, query))
+
+    def request_slack(self, msg):
+        # 슬랙 전송
+        headers = {"Content-Type": "application/json"}
+        data = f"{{'text':'{msg}', 'token':'{self.slack_token}'}}"
+        data = data.encode(encoding='utf-8')
+        requests.post(self.slack_hook_token, data=data, headers=headers)
+
+    def commit_fairy(self):
+        git_data =  self.request_github()
+        self.request_slack(git_data['data']['user']['contributionsCollection']['totalCommitContributions'])
 
 if __name__ == "__main__":
-    git_data =  run_query(query)
-    print(git_data['data']['user']['contributionsCollection']['totalCommitContributions'])
-    send_slack(git_data['data']['user']['contributionsCollection']['totalCommitContributions'])
+    u = UserInfo()
+    u.commit_fairy()
